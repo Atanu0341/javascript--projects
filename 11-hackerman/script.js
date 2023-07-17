e18051be7dc5adab4e816cfcd243f7aca0bcba0b
Atanu Majumder <atanumajumder2004@gmail.com>
2023-07-17 13:05:11 +0530

    project 11 added

---
+++ 11-hackerman/script.js
@@ -0,0 +1,39 @@
+let a = [
+  "Initializing Hack tool...",
+  "Connecting to Facebook...",
+  "Connecting to server 1...",
+  "Connection failed. Retrying...",
+  "Connecting to server 2",
+  "Connected Successfully...",
+  "Username d_e_b_j_i_t_559...",
+  "Trying Brute Force...",
+  "200K passwords tried...",
+  "Match not found",
+  "Another 200K passwords tried...",
+  "Match not found...",
+  "Another 200K passwords tried...",
+  "Match not found...",
+  "Another 200K passwords tried...",
+  "Match found...",
+  "Accessing Account...",
+  "Hack Successful...",
+];
+
+
+const sleep=async(seconds)=>{
+    return new Promise((resolve, reject)=>{
+        setTimeout(()=>{resolve(true)},seconds*1000)
+    })
+}
+
+const showHack=async(messages)=>{
+    await sleep(2)
+    // console.log(messages)
+    text.innerHTML = text.innerHTML + messages + "<br>"
+}
+
+(async () => {
+    for (let i = 0; i < a.length; i++) {
+            await showHack(a[i])
+    }
+})()
